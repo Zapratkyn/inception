7f22aedf5825b0cca692d7347d2b9829a60e35fd
gponcele <gilles.ponpon@gmail.com>
2023-09-24 20:17:08 +0200

    Update auto-config.sh

--- srcs/requirements/wordpress/tools/auto-config.sh
+++ srcs/requirements/wordpress/tools/auto-config.sh
@@ -2,7 +2,8 @@
 
 sleep 10
 
-if [ ! -e /var/www/wordpress/wp-config.php]; then
+if [ ! -e /var/www/wordpress/wp-config.php ] 
+then
 	wp config create --allow-root \
 		--dbname=$SQL_DATABASE \
 		--dbuser=$SQL_USER \
@@ -29,6 +30,8 @@ if [ ! -e /var/www/wordpress/wp-config.php]; then
 
 fi
 
-if [ ! -d /run/php ]; then
+if [ ! -d /run/php ] 
+then
 	mkdir ./run/php
 fi
+/usr/sbin/php-fpm7.3 -F
