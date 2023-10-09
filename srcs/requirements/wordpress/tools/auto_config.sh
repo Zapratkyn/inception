86d23483cfccf16734f4443000a7809003fb34e7
root <gilles.ponpon@gmail.com>
2023-10-09 14:37:20 +0200

    MAJ

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,37 +1,20 @@
 #!bin/bash
 
-sleep 10
+sleep 5
 
-sed -i "s|listen = /run/php/php7.4-fpm|sock|listen = 9000|g" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/7.4/fpm/pool.d/www.conf
 
-if [ ! -e /var/www/wordpress/wp-config.php ] 
-then
+ls -la
+
+if [ ! -f ./wp-config.php ]; then
 
 	wp core download --allow-root --path=/var/www/wordpress/
 
-	wp config create --allow-root \
-		--dbname=$SQL_DATABASE \
-		--dbuser=$SQL_USER \
-		--dbpass=$SQL_PASSWORD \
-		--dbhost=mariadb \
-	       	--path='/var/www/wordpress'
+	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --path='/var/www/wordpress'
 
-	sleep 2
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 
-	wp core install --url=$DOMAIN_NAME \
-		--title=$SITE_TITLE \
-		--admin_user=$ADMIN_USER \
-		--admin_password=$ADMIN_PASSWORD \
-		--admin_email=$ADMIN_EMAIL \
-		--allow-root \
-		--path='/var/www/wordpress'
-
-	wp user create --allow-root \
-		--role=author \
-		$USER1_LOGIN \
-		$USER1_MAIL \
-		--user_pass=$USER1_PASSWORD \
-		--path='/var/www/wordpress' >> /log.txt
+	wp user create --allow-root $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASSWORD
 
 else
 	echo "Wordpress is already installed and configured"
